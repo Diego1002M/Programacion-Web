477e3a46511ba3d1acc05686e0b17ef03b012b5b
Diego1002M <60741323@continental.edu.pe>
2025-11-20 12:42:48 -0500

    a
    a

--- FRONTEND/src/app/componente/pagina3/pagina3.ts
+++ FRONTEND/src/app/componente/pagina3/pagina3.ts
@@ -19,17 +19,19 @@ export class Pagina3 {
 
   opcionSeleccionada: number | null = null;
 
-  opciones = [
-    { img: 'assets/alertas/robo.webp', texto: 'Robo o asalto' },
-    { img: 'assets/alertas/incendio.avif', texto: 'Incendio' },
-    { img: 'assets/alertas/accidente.png', texto: 'Accidente de tránsito' },
-    { img: 'assets/alertas/violencia.png', texto: 'Violencia doméstica' },
-    { img: 'assets/alertas/emergencia.png', texto: 'Emergencia médica' },
-    { img: 'assets/alertas/Sospechoso.png', texto: 'Persona sospechosa' },
-    { img: 'assets/alertas/Secuestro.png', texto: 'Secuestro' },
-    { img: 'assets/alertas/Extorsión.png', texto: 'Extorsión' },
-    { img: 'assets/alertas/Otros.png', texto: 'Otro tipo de emergencia' }
-  ];
+opciones = [
+  { img: 'assets/alertas/robo.webp', texto: 'Robo o asalto' },
+  { img: 'assets/alertas/incendio.avif', texto: 'Incendio' },
+  { img: 'assets/alertas/accidente.png', texto: 'Accidente de tránsito' },
+  { img: 'assets/alertas/violencia.png', texto: 'Violencia doméstica' },
+  { img: 'assets/alertas/emergencia.png', texto: 'Emergencia médica' },
+  { img: 'assets/alertas/sospechoso.png', texto: 'Persona sospechosa' },
+  { img: 'assets/alertas/secuestro.png', texto: 'Secuestro' },
+  { img: 'assets/alertas/extorsion.png', texto: 'Extorsión' },
+  { img: 'assets/alertas/otros.png', texto: 'Otro tipo de emergencia' }
+];
+
+
 
   seleccionarOpcion(index: number) {
     this.opcionSeleccionada = index;
